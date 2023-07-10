08501c4978ae53dc2c002d010b0f6a9b6c5b1d3d
Nursyahidatul <nursyahidatulasyiqin@graduate.utm.my>
2023-07-10 23:53:45 +0800

    Multiplayer character selection

--- Assets/Script/SpawnPlayerMP.cs
+++ Assets/Script/SpawnPlayerMP.cs
@@ -8,17 +8,57 @@ using TMPro;
 public class SpawnPlayerMP : MonoBehaviourPunCallbacks
 {
     public string[] playerName;
-    public GameObject player;
+    public GameObject[] playerPrefabs;
     public Vector3[] playerPos;
+    public Button[] characterSelectionButtons;
 
     // Start is called before the first frame update
     void Start()
+    {
+        for(int i = 0; i < characterSelectionButtons.Length; i++)
+        {
+            int characterIndex = i; // Capture the index for the callback
+            characterSelectionButtons[i].onClick.AddListener(() => OnCharacterSelectionButtonClicked(characterIndex));
+        }
+    }
+
+    public void OnCharacterSelectionButtonClicked(int index)
     {
         if (PhotonNetwork.IsMasterClient)
         {
             playerName[0] = PhotonNetwork.NickName;
             photonView.RPC("Set_OtherPlayerName", RpcTarget.OthersBuffered, 0, PhotonNetwork.NickName);
-            PhotonNetwork.Instantiate(player.name, playerPos[0], Quaternion.identity);
+            SpawnPlayerWithCharacter(index, playerPos[0], Quaternion.identity);
+        }
+        else
+        {
+            playerName[1] = PhotonNetwork.NickName;
+            photonView.RPC("Set_OtherPlayerName", RpcTarget.OthersBuffered, 1, PhotonNetwork.NickName);
+            Quaternion rotationB = Quaternion.Euler(0f, 180f, 0f);
+            SpawnPlayerWithCharacter(index, playerPos[1], rotationB);
+        }
+
+        // Disable the canvas game object
+        characterSelectionButtons[index].transform.parent.gameObject.SetActive(false);
+    }
+
+    void SpawnPlayerWithCharacter(int characterIndex, Vector3 position, Quaternion rotation)
+    {
+        GameObject selectedCharacterPrefab = playerPrefabs[characterIndex];
+        GameObject playerObject = PhotonNetwork.Instantiate(selectedCharacterPrefab.name, position, rotation);
+       /* UpdatePlayerName(playerObject, playerName[characterIndex]);*/
+    }
+
+/*    // Start is called before the first frame update
+    void Start()
+    {
+        int selectedCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter");
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            playerName[0] = PhotonNetwork.NickName;
+            photonView.RPC("Set_OtherPlayerName", RpcTarget.OthersBuffered, 0, PhotonNetwork.NickName);
+            PhotonNetwork.Instantiate(playerPrefabs[selectedCharacterIndex].name, playerPos[0], Quaternion.identity);
             //Transform canvas = player.transform.Find("Canvas");
             //Transform showName = canvas.Find("Name");
             //showName.GetComponent<TextMeshProUGUI>().text = playerName[0];
@@ -28,18 +68,12 @@ public class SpawnPlayerMP : MonoBehaviourPunCallbacks
             playerName[1] = PhotonNetwork.NickName;
             photonView.RPC("Set_OtherPlayerName", RpcTarget.OthersBuffered, 1, PhotonNetwork.NickName);
             Quaternion rotationB = Quaternion.Euler(0f, 180f, 0f);
-            PhotonNetwork.Instantiate(player.name, playerPos[1], rotationB);
+            PhotonNetwork.Instantiate(playerPrefabs[selectedCharacterIndex].name, playerPos[1], rotationB);
             //Transform canvas = player.transform.Find("Canvas");
             //Transform showName = canvas.Find("Name");
             //showName.GetComponent<TextMeshProUGUI>().text = playerName[1];
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    }*/
 
     [PunRPC]
     void Set_OtherPlayerName(int index, string name)
